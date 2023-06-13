9e39619ef2baaeb5a7bd956a6cda04079dbf0c9e
ajay <ajay.mahehswari@getvymo.com>
2023-06-13 08:40:41 +0530

    added chakra ui with basic card layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import App from "./App";
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
